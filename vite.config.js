a21cbaa549a374955e0a8787beab0041785fbe7c
Lutfi Nailufar Nurdin <lutfi.nurdin@sigma.co.id>
2024-12-31 10:48:16 +0700

    initial

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Amalan Rajab',
+        short_name: 'AmalanRajab',
+        description: 'Amalan Rajab Haqqani',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
