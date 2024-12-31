535b4a2c005c89319e735bf594003aabe0381d57
Lutfi Nailufar Nurdin <lutfi.nurdin@sigma.co.id>
2024-12-31 10:58:20 +0700

    publish github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/amalan-rajab/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
